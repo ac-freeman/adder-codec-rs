c1d8db3ff0dea42106c8f9bcde31983d0a7768f6
Andrew <afreema4@live.unc.edu>
2022-10-19 08:44:42 -0400

    Update download script

--- evaluation/mmsys23/framed_to_adder/dataset/download_dataset.sh
+++ evaluation/mmsys23/framed_to_adder/dataset/download_dataset.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+## Download the Youtube UGC Datset
+## (https://media.withyoutube.com/)
+
+## Usage:
+# download_dataset.sh /path/to/store/dataset
+
 # Need to first install gsutil
 # Ubuntu 20.04: sudo snap install google-cloud-cli --classic
 DATASET_STORE_PATH=$1   # Where to store the dataset locally
