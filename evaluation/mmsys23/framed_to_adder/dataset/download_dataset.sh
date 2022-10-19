ebd5b077ee89976d5f9d9d370856b1fd3a004933
Andrew <afreema4@live.unc.edu>
2022-10-19 08:39:53 -0400

    Update download script

--- evaluation/mmsys23/framed_to_adder/dataset/download_dataset.sh
+++ evaluation/mmsys23/framed_to_adder/dataset/download_dataset.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
-gsutil cp ugc-dataset/original_videos_h264 .
+# Need to first install gsutil
+# Ubuntu 20.04: sudo snap install google-cloud-cli --classic
+DATASET_STORE_PATH=$1   # Where to store the dataset locally
+mkdir -p "$DATASET_STORE_PATH";   # Make the directory, if it doesn't exist
+gsutil cp -r gs://ugc-dataset/original_videos_h264 "$DATASET_STORE_PATH"    # Download the dataset with gsutil
