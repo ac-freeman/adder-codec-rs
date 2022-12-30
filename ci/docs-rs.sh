8bd558baf33fa9d81db6ea826eb5759e8b7d7a6b
Andrew <afreema4@live.unc.edu>
2022-12-30 15:20:01 -0500

    Install more deps

--- ci/docs-rs.sh
+++ ci/docs-rs.sh
@@ -5,6 +5,8 @@ set -vex
 sudo apt-get update
 sudo apt-get -y install clang
 sudo apt-get -y install libsdl2-dev
+sudo apt-get -y install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+sudo apt-get -y install g++ pkg-config libx11-dev libasound2-dev libudev-dev
 
 
 # workaround to make clang_sys crate detect installed libclang
