cee108d12b02b9b463574a940f461f25bf072d4f
Andrew <afreema4@live.unc.edu>
2023-03-22 13:02:22 -0400

    Inter tests passing

--- adder-codec-core/src/codec/compressed/adu/interblock.rs
+++ adder-codec-core/src/codec/compressed/adu/interblock.rs
@@ -145,165 +145,154 @@ fn decompress_dt_residuals<R: Read>(
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::codec::compressed::adu::interblock::AduInterBlock;
-//     use crate::codec::compressed::adu::intrablock::gen_random_intra_block;
-//     use crate::codec::compressed::adu::{AduComponentCompression, AduCompression};
-//     use crate::codec::compressed::blocks::{BLOCK_SIZE_AREA, D_ENCODE_NO_EVENT};
-//     use crate::codec::compressed::stream::{CompressedInput, CompressedOutput};
-//     use crate::codec::encoder::Encoder;
-//     use crate::codec::{CodecMetadata, WriteCompression};
-//     use std::error::Error;
-//     use std::io::BufReader;
-//
-//     fn setup_encoder() -> crate::codec::compressed::stream::CompressedOutput<Vec<u8>> {
-//         let meta = CodecMetadata {
-//             delta_t_max: 100,
-//             ref_interval: 100,
-//             ..Default::default()
-//         };
-//         // By building the CompressedOutput directly (rather than calling Encoder::new_compressed),
-//         // we can avoid writing the header and stuff for testing purposes.
-//         crate::codec::compressed::stream::CompressedOutput::new(meta, Vec::new())
-//     }
-//
-//     fn compress_inter_block() -> Result<(AduInterBlock, Vec<u8>), Box<dyn Error>> {
-//         let mut encoder = setup_encoder();
-//         let intra_block = gen_random_intra_block(1234, encoder.meta.delta_t_max, Some(7));
-//         // For convenience, we'll just use the intra block's generator.
-//         let inter_block = AduInterBlock {
-//             shift_loss_param: intra_block.shift_loss_param,
-//             d_residuals: intra_block.d_residuals,
-//             t_residuals: intra_block.dt_residuals,
-//         };
-//
-//         assert!(inter_block
-//             .compress(
-//                 encoder.arithmetic_coder.as_mut().unwrap(),
-//                 encoder.contexts.as_mut().unwrap(),
-//                 encoder.stream.as_mut().unwrap(),
-//                 encoder.meta.delta_t_max
-//             )
-//             .is_ok());
-//
-//         let written_data = encoder.into_writer().unwrap();
-//
-//         Ok((inter_block, written_data))
-//     }
-//
-//     #[test]
-//     fn test_compress_inter_block() {
-//         let (_, written_data) = compress_inter_block().unwrap();
-//         let output_len = written_data.len();
-//         let input_len = 1028; // Rough approximation
-//         assert!(output_len < input_len);
-//         eprintln!("Written data: {:?}", written_data);
-//     }
-//
-//     #[test]
-//     fn test_decompress_inter_block() {
-//         let (inter_block, written_data) = compress_inter_block().unwrap();
-//         let tmp_len = written_data.len();
-//
-//         let mut bufreader = BufReader::new(written_data.as_slice());
-//         let mut bitreader =
-//             bitstream_io::BitReader::endian(&mut bufreader, bitstream_io::BigEndian);
-//
-//         let mut decoder = CompressedInput::new(100, 100);
-//
-//         let decoded_inter_block = AduInterBlock::decompress(&mut bitreader, &mut decoder);
-//
-//         decoder
-//             .arithmetic_coder
-//             .as_mut()
-//             .unwrap()
-//             .model
-//             .set_context(decoder.contexts.as_mut().unwrap().eof_context);
-//         let eof = decoder
-//             .arithmetic_coder
-//             .as_mut()
-//             .unwrap()
-//             .decode(&mut bitreader)
-//             .unwrap();
-//         assert!(eof.is_none());
-//         assert_eq!(
-//             inter_block.shift_loss_param,
-//             decoded_inter_block.shift_loss_param
-//         );
-//         assert_eq!(inter_block.d_residuals, decoded_inter_block.d_residuals);
-//         assert_eq!(inter_block.t_residuals, decoded_inter_block.t_residuals);
-//     }
-//
-//     #[test]
-//     fn test_decompress_mostly_empty_interblock() {
-//         let mut encoder = CompressedOutput::new(
-//             CodecMetadata {
-//                 codec_version: 0,
-//                 header_size: 0,
-//                 time_mode: Default::default(),
-//                 plane: Default::default(),
-//                 tps: 0,
-//                 ref_interval: 255,
-//                 delta_t_max: 102000,
-//                 event_size: 0,
-//                 source_camera: Default::default(),
-//             },
-//             Vec::new(),
-//         );
-//
-//         let dtm = encoder.meta.delta_t_max;
-//         let ref_interval = encoder.meta.ref_interval;
-//         // let mut encoder: Encoder<Vec<u8>> = Encoder::new_compressed(compression);
-//
-//         let mut d_residuals = [D_ENCODE_NO_EVENT; BLOCK_SIZE_AREA];
-//         d_residuals[39] = -2;
-//
-//         let mut t_residuals = [0; BLOCK_SIZE_AREA];
-//         t_residuals[39] = -1;
-//         let reference_block = AduInterBlock {
-//             shift_loss_param: 7,
-//             d_residuals,
-//             t_residuals,
-//         };
-//
-//         assert!(reference_block
-//             .compress(
-//                 encoder.arithmetic_coder.as_mut().unwrap(),
-//                 encoder.contexts.as_mut().unwrap(),
-//                 encoder.stream.as_mut().unwrap(),
-//                 encoder.meta.delta_t_max
-//             )
-//             .is_ok());
-//
-//         let written_data = encoder.into_writer().unwrap();
-//
-//         let mut bufreader = BufReader::new(written_data.as_slice());
-//         let mut bitreader =
-//             bitstream_io::BitReader::endian(&mut bufreader, bitstream_io::BigEndian);
-//
-//         let mut decoder = CompressedInput::new(dtm, ref_interval);
-//
-//         let decoded_inter_block = AduInterBlock::decompress(&mut bitreader, &mut decoder);
-//
-//         decoder
-//             .arithmetic_coder
-//             .as_mut()
-//             .unwrap()
-//             .model
-//             .set_context(decoder.contexts.as_mut().unwrap().eof_context);
-//         let eof = decoder
-//             .arithmetic_coder
-//             .as_mut()
-//             .unwrap()
-//             .decode(&mut bitreader)
-//             .unwrap();
-//         assert!(eof.is_none());
-//         assert_eq!(
-//             reference_block.shift_loss_param,
-//             decoded_inter_block.shift_loss_param
-//         );
-//         assert_eq!(reference_block.d_residuals, decoded_inter_block.d_residuals);
-//         assert_eq!(reference_block.t_residuals, decoded_inter_block.t_residuals);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::codec::compressed::adu::interblock::AduInterBlock;
+    use crate::codec::compressed::adu::intrablock::gen_random_intra_block;
+    use crate::codec::compressed::adu::{add_eof, AduComponentCompression, AduCompression};
+    use crate::codec::compressed::blocks::{BLOCK_SIZE_AREA, D_ENCODE_NO_EVENT};
+    use crate::codec::compressed::stream::{CompressedInput, CompressedOutput};
+    use crate::codec::encoder::Encoder;
+    use crate::codec::{CodecMetadata, WriteCompression};
+    use std::error::Error;
+    use std::io::{BufReader, Cursor};
+
+    fn setup_encoder() -> crate::codec::compressed::stream::CompressedOutput<Vec<u8>> {
+        let meta = CodecMetadata {
+            delta_t_max: 100,
+            ref_interval: 100,
+            ..Default::default()
+        };
+        // By building the CompressedOutput directly (rather than calling Encoder::new_compressed),
+        // we can avoid writing the header and stuff for testing purposes.
+        crate::codec::compressed::stream::CompressedOutput::new(meta, Vec::new())
+    }
+
+    fn compress_inter_block() -> Result<(AduInterBlock, Vec<u8>), Box<dyn Error>> {
+        let mut encoder = setup_encoder();
+        let intra_block = gen_random_intra_block(1234, encoder.meta.delta_t_max, Some(7));
+        // For convenience, we'll just use the intra block's generator.
+        let inter_block = AduInterBlock {
+            shift_loss_param: intra_block.shift_loss_param,
+            d_residuals: intra_block.d_residuals,
+            t_residuals: intra_block.dt_residuals,
+        };
+
+        assert!(inter_block
+            .compress(
+                encoder.arithmetic_coder.as_mut().unwrap(),
+                encoder.contexts.as_mut().unwrap(),
+                encoder.stream.as_mut().unwrap(),
+                encoder.meta.delta_t_max
+            )
+            .is_ok());
+
+        add_eof(&mut encoder);
+
+        let written_data = encoder.into_writer().unwrap();
+
+        Ok((inter_block, written_data))
+    }
+
+    #[test]
+    fn test_compress_inter_block() {
+        let (_, written_data) = compress_inter_block().unwrap();
+        let output_len = written_data.len();
+        let input_len = 1028; // Rough approximation
+        assert!(output_len < input_len);
+        eprintln!("Written data: {:?}", written_data);
+    }
+
+    #[test]
+    fn test_decompress_inter_block() {
+        let (inter_block, written_data) = compress_inter_block().unwrap();
+        let tmp_len = written_data.len();
+
+        let mut bufreader = Cursor::new(written_data);
+        let mut bitreader = bitstream_io::BitReader::endian(bufreader, bitstream_io::BigEndian);
+
+        let mut compressed_input: CompressedInput<Cursor<Vec<u8>>> = CompressedInput::new(100, 100);
+        let mut decoder = compressed_input.arithmetic_coder.as_mut().unwrap();
+        let mut contexts = compressed_input.contexts.as_mut().unwrap();
+
+        let decoded_inter_block =
+            AduInterBlock::decompress(&mut decoder, &mut contexts, &mut bitreader, 100);
+
+        decoder.model.set_context(contexts.eof_context);
+        let eof = decoder.decode(&mut bitreader).unwrap();
+        assert!(eof.is_none());
+        assert_eq!(
+            inter_block.shift_loss_param,
+            decoded_inter_block.shift_loss_param
+        );
+        assert_eq!(inter_block.d_residuals, decoded_inter_block.d_residuals);
+        assert_eq!(inter_block.t_residuals, decoded_inter_block.t_residuals);
+    }
+
+    #[test]
+    fn test_decompress_mostly_empty_interblock() {
+        let mut encoder = CompressedOutput::new(
+            CodecMetadata {
+                codec_version: 0,
+                header_size: 0,
+                time_mode: Default::default(),
+                plane: Default::default(),
+                tps: 0,
+                ref_interval: 255,
+                delta_t_max: 102000,
+                event_size: 0,
+                source_camera: Default::default(),
+            },
+            Vec::new(),
+        );
+
+        let dtm = encoder.meta.delta_t_max;
+        let ref_interval = encoder.meta.ref_interval;
+        // let mut encoder: Encoder<Vec<u8>> = Encoder::new_compressed(compression);
+
+        let mut d_residuals = [D_ENCODE_NO_EVENT; BLOCK_SIZE_AREA];
+        d_residuals[39] = -2;
+
+        let mut t_residuals = [0; BLOCK_SIZE_AREA];
+        t_residuals[39] = -1;
+        let reference_block = AduInterBlock {
+            shift_loss_param: 7,
+            d_residuals,
+            t_residuals,
+        };
+
+        assert!(reference_block
+            .compress(
+                encoder.arithmetic_coder.as_mut().unwrap(),
+                encoder.contexts.as_mut().unwrap(),
+                encoder.stream.as_mut().unwrap(),
+                encoder.meta.delta_t_max
+            )
+            .is_ok());
+
+        add_eof(&mut encoder);
+
+        let written_data = encoder.into_writer().unwrap();
+
+        let mut bufreader = Cursor::new(written_data);
+        let mut bitreader = bitstream_io::BitReader::endian(bufreader, bitstream_io::BigEndian);
+
+        let mut compressed_input: CompressedInput<Cursor<Vec<u8>>> =
+            CompressedInput::new(dtm, ref_interval);
+        let mut decoder = compressed_input.arithmetic_coder.as_mut().unwrap();
+        let mut contexts = compressed_input.contexts.as_mut().unwrap();
+
+        let decoded_inter_block =
+            AduInterBlock::decompress(&mut decoder, &mut contexts, &mut bitreader, dtm);
+
+        decoder.model.set_context(contexts.eof_context);
+        let eof = decoder.decode(&mut bitreader).unwrap();
+        assert!(eof.is_none());
+        assert_eq!(
+            reference_block.shift_loss_param,
+            decoded_inter_block.shift_loss_param
+        );
+        assert_eq!(reference_block.d_residuals, decoded_inter_block.d_residuals);
+        assert_eq!(reference_block.t_residuals, decoded_inter_block.t_residuals);
+    }
+}
