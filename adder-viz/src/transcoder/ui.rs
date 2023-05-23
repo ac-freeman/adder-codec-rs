651eebe2a967f61bd6b2691f2e245ce60a9b620f
Eric Schneider <u1643364@live.unc.edu>
2023-05-22 23:23:06 -0400

    Combine filters

--- adder-viz/src/transcoder/ui.rs
+++ adder-viz/src/transcoder/ui.rs
@@ -153,8 +153,7 @@ impl TranscoderState {
         ui.horizontal(|ui| {
             if ui.button("Open file").clicked() {
                 if let Some(path) = rfd::FileDialog::new()
-                    .add_filter("framed video", &["mp4"])
-                    .add_filter("DVS/DAVIS video", &["aedat4"])
+                    .add_filter("video", &["mp4", "aedat4"])
                     .pick_file()
                 {
                     self.ui_info_state.input_path_0 = Some(path.clone());
