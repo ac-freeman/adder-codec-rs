a8441117c938df7116a89233e7ddbd7aacdb0c9c
Andrew <afreema4@live.unc.edu>
2023-10-25 14:38:13 -0400

    Test encoding and decoding several Adus in succession

--- adder-codec-core/src/codec/compressed/stream.rs
+++ adder-codec-core/src/codec/compressed/stream.rs
@@ -211,7 +211,7 @@ impl<R: Read> ReadCompression<R> for CompressedInput<R> {
             if adu.decoder_is_empty() {
                 // Read the size of the Adu in bytes
                 let mut buffer = [0u8; 4];
-                reader.read_bytes(&mut buffer).unwrap();
+                reader.read_bytes(&mut buffer)?;
                 let num_bytes = u32::from_be_bytes(buffer);
 
                 // Read the compressed Adu from the stream
@@ -252,11 +252,12 @@ impl<R: Read> ReadCompression<R> for CompressedInput<R> {
 #[cfg(test)]
 mod tests {
     use crate::codec::compressed::stream::CompressedInput;
-    use crate::codec::{ReadCompression, WriteCompression};
+    use crate::codec::{CodecError, ReadCompression, WriteCompression};
     use crate::PlaneSize;
     use bitstream_io::{BigEndian, BitReader};
     use std::cmp::min;
     use std::error::Error;
+    use std::io;
 
     /// Test the creation a CompressedOutput and writing a bunch of events to it but NOT getting
     /// to the time where we compress the Adu
@@ -404,6 +405,95 @@ mod tests {
         }
 
         assert_eq!(input_px_events, output_px_events);
+        Ok(())
+    }
+
+    #[test]
+    fn test_compress_decompress_several() -> Result<(), Box<dyn Error>> {
+        use crate::codec::compressed::stream::CompressedOutput;
+        use crate::codec::raw::stream::RawOutput;
+        use crate::codec::WriteCompression;
+        use crate::Coord;
+        use crate::{Event, EventCoordless, SourceCamera, TimeMode};
+        use std::io::Cursor;
+
+        let plane = PlaneSize::new(16, 30, 1)?;
+        let start_t = 0;
+        let dt_ref = 255;
+        let num_intervals = 10;
+
+        // A random candidate pixel to check that its events match
+        let mut candidate_px_idx = (7, 12);
+        let mut input_px_events = Vec::new();
+        let mut output_px_events = Vec::new();
+
+        let mut compressed_output = CompressedOutput::new(
+            crate::codec::CodecMetadata {
+                codec_version: 0,
+                header_size: 0,
+                time_mode: TimeMode::AbsoluteT,
+                plane: PlaneSize {
+                    width: 16,
+                    height: 32,
+                    channels: 1,
+                },
+                tps: 7650,
+                ref_interval: dt_ref,
+                delta_t_max: dt_ref * num_intervals as u32,
+                event_size: 0,
+                source_camera: SourceCamera::FramedU8,
+            },
+            Cursor::new(Vec::new()),
+        );
+
+        let mut counter = 0;
+        for i in 0..10 {
+            for y in 0..30 {
+                for x in 0..16 {
+                    let event = Event {
+                        coord: Coord { x, y, c: None },
+                        t: 280 + counter,
+                        d: 7,
+                    };
+                    if y == candidate_px_idx.0 && x == candidate_px_idx.1 {
+                        dbg!(event.clone());
+                        input_px_events.push(event);
+                    }
+                    compressed_output.ingest_event(event)?;
+
+                    counter += 1;
+                }
+            }
+        }
+
+        let output = compressed_output.into_writer().unwrap().into_inner();
+        assert!(!output.is_empty());
+        dbg!(counter);
+        // Check that the size is less than the raw events
+        assert!((output.len() as u32) < counter * 9);
+
+        let mut compressed_input = CompressedInput::new(dt_ref * num_intervals as u32, dt_ref);
+        compressed_input.meta.plane = plane;
+        let mut stream = BitReader::endian(Cursor::new(output), BigEndian);
+        for i in 0..counter - 1 {
+            dbg!(i);
+            match compressed_input.digest_event(&mut stream) {
+                Ok(event) => {
+                    if event.coord.y == candidate_px_idx.0 && event.coord.x == candidate_px_idx.1 {
+                        dbg!(event.clone(), i);
+                        output_px_events.push(event);
+                    }
+                }
+                Err(CodecError::IoError(e)) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+
+                Err(e) => return Err(Box::new(e)),
+            }
+        }
+
+        assert!(input_px_events.len() >= output_px_events.len());
+        for i in 0..output_px_events.len() {
+            assert_eq!(input_px_events[i], output_px_events[i]);
+        }
         Ok(())
     }
 }
