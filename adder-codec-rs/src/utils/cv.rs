660c00f48258dc1f79326cd3e502d2610cf07310
Andrew Freeman <afreema4@live.unc.edu>
2024-04-08 18:07:50 -0400

    Fix inf davis crash

--- adder-codec-rs/src/utils/cv.rs
+++ adder-codec-rs/src/utils/cv.rs
@@ -429,11 +429,10 @@ pub fn clamp_u8(frame_val: &mut f64, last_val_ln: &mut f64) {
     if *frame_val <= 0.0 {
         *frame_val = 0.0;
         *last_val_ln = 0.0_f64.ln_1p();
+    } else if *frame_val > 255.0 {
+        *frame_val = 255.0;
+        *last_val_ln = 1.0_f64.ln_1p();
     }
-    // else if *frame_val > 255.0 {
-    //     *frame_val = 255.0;
-    //     *last_val_ln = 1.0_f64.ln_1p();
-    // }
 }
 
 /// Clamp the value to the range [0, 255]. If the value is outside of this range, set it to a mid-
