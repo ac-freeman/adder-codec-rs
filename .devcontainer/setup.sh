03d0dfb1e2ad250a6a4ee095570fb4d32a632666
Daniel Eades <danieleades@hotmail.com>
2024-09-16 21:17:33 +0100

    add dependencies to devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,10 @@
-sudo apt-get update
-sudo apt-get install -y \
-    libgtk-3-dev
+rustup install nightly
+sudo apt-get update && sudo apt-get install -y \
+    clang \
+    libavdevice-dev \
+    libavfilter-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libclang-dev \
+    libgtk-3-dev \
+    libopencv-dev
