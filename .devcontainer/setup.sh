c3ef8c0560a51e853724203dba75cd1f10ead19b
danieleades <33452915+danieleades@users.noreply.github.com>
2025-01-15 15:51:40 +0000

    Add devcontainer (#125)
    * add basic devcontainer config
    
    * add dependencies to devcontainer
    
    ---------
    
    Co-authored-by: daniel.eades <daniel.eades@seebyte.com>

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+rustup install nightly
+sudo apt-get update && sudo apt-get install -y \
+    clang \
+    libavdevice-dev \
+    libavfilter-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libclang-dev \
+    libgtk-3-dev \
+    libopencv-dev
